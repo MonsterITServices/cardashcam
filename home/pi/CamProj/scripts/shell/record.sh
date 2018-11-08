c9537b13ac2a0e41b550d8c7a8212c100c9e6168
Alex Tovey <alex@monster-it.co.uk>
2018-11-08 13:42:18 +0000

    Update to show black bacground white text
    White text and black background on video

--- home/pi/CamProj/scripts/shell/record.sh
+++ home/pi/CamProj/scripts/shell/record.sh
@@ -13,7 +13,7 @@ do
 	echo "New FileName: " "$new_fileName"
 	
 	#This was the toughest part of the whole project to find the optimal settings for recording.
-	raspivid -a 12 -ae 32,0xff,0x808000 -o $new_fileName -w 1280 -h 720 -t 1800000
+	raspivid -a 1036 -o $new_fileName -w 1280 -h 720 -t 1800000
 
 	echo "Done with recording..."
 
