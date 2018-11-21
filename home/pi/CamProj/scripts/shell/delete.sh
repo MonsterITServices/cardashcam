5fda681abb036448ecbe9c61e368f95839c8ab06
Alex Tovey <alex@monster-it.co.uk>
2018-11-21 10:41:49 +0000

    changed date format
    changed date format and also the extension which to look for

--- home/pi/CamProj/scripts/shell/delete.sh
+++ home/pi/CamProj/scripts/shell/delete.sh
@@ -3,7 +3,7 @@
 file_logs_name=delete_logs_
 file_video_name=delete_video_
 
-current_time=$(date "+%b-%d-%Y__%H.%M.%S")
+current_time=$(date "+%d-%b-%Y__%H.%M.%S")
 	echo "Current Time : $current_time"
 
 new_log_fileName=$file_logs_name$current_time.log
@@ -16,5 +16,5 @@ echo "New FileName: " "$new_video_fileName"
  
 sudo find /home/pi/CamProj/logs/python/ /home/pi/CamProj/logs/shell/ -mtime +30 -type f -name "*.log" -delete >> /home/pi/CamProj/logs/shell/$new_log_fileName 2>&1
 
-sudo find /home/pi/CamProj/video/ -mtime +30 -type f -name "*.mp4" -delete >> /home/pi/CamProj/logs/shell/$new_video_fileName 2>&1
+sudo find /home/pi/CamProj/video/ -mtime +30 -type f -name "*.264" -delete >> /home/pi/CamProj/logs/shell/$new_video_fileName 2>&1
 
